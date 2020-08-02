fda255d7e4b9b9343dd26493e6e6524a4dfc23b1
Arka Barua <arka.barua83@gamil.com>
2020-08-02 18:02:42 +0600

    I am learning variable

---
+++ variable.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+   int var = 65;
+    printf("var+5=%d\n",var+5);
+    printf("var-5=%d\n",var-5);
+    printf("var/5=%d\n",var/5);
+    printf("var*5=%d\n",var*5);
+    printf("var=%d\n",var);
+
+
+      int a=50;
+      int b=10;
+      printf("%d + %d = %d\n",a,b,a+b);
+      printf("%d - %d = %d\n",a,b,a-b);
+      printf("%d * %d = %d\n",a,b,a*b);
+      printf("%d / %d = %d\n",a,b,a/b);
+
+//defined a variable.
+      int c;
+      //declared a variable.
+      c=500;
+      c=100;
+      printf("%d\n",c);
+    return 0;
+
+}
+//variable name also called identifier.
+//Global variable.
+//Local varible.
